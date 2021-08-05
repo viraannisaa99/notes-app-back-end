2f9d5ef32abc56dc55d97bf8ec70fd85e25d349a
viraannisaa99 <viraannisaa99@gmail.com>
2021-08-05 11:30:58 +0700

    postgres database implemented

---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+/**
+ * untuk menyesuaikan kemballi struktur database dengan cara mapping objek.
+ */
+
+const mapDBToModel = ({ id, title, body, tags, created_at, updated_at }) => ({
+  id,
+  title,
+  body,
+  tags,
+  createdAt: created_at, // perbaiki properti agar strukturnya sama dan tidak gagal di pengujian
+  updatedAt: updated_at,
+});
+
+module.exports = { mapDBToModel };
